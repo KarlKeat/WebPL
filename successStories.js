727f92be605cc30da6103bc3a5764325ec5f75a3
V L <vl4kz@virginia.edu>
2017-09-27 14:03:55 -0400

    finished successStories.html page, created javascript to advance the slideshow, description appears upon hover

---
+++ successStories.js
@@ -0,0 +1,19 @@
+/*
+https://www.w3schools.com/w3css/w3css_slideshow.asp
+*/
+var slideIndex = 1;
+
+function changeSlides(n) {
+  showSlideshow(slideIndex += n);
+}
+
+function showSlideshow(index) {
+  var i;
+  var slideshow = document.getElementsByClassName("content");
+  if (index > slideshow.length) {slideIndex = 1}
+  if (index < 1) {slideIndex = slideshow.length}
+  for (i = 0; i < slideshow.length; i++) {
+     slideshow[i].style.display = "none";
+  }
+  slideshow[slideIndex-1].style.display = "block";
+}
