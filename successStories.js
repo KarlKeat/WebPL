d46dff94faa80bc9c522d8daf9388a5034d93a4e
V L <vl4kz@virginia.edu>
2017-09-27 15:43:35 -0400

    Changed z-index value for footer, fixed slideshow

--- successStories.js
+++ successStories.js
@@ -1,7 +1,7 @@
 /*
 https://www.w3schools.com/w3css/w3css_slideshow.asp
 */
-var slideIndex = 1;
+var slideIndex = 0;
 
 function changeSlides(n) {
   showSlideshow(slideIndex += n);
@@ -10,10 +10,18 @@ function changeSlides(n) {
 function showSlideshow(index) {
   var i;
   var slideshow = document.getElementsByClassName("content");
-  if (index > slideshow.length) {slideIndex = 1}
-  if (index < 1) {slideIndex = slideshow.length}
+
+  if (index > slideshow.length) {
+    slideIndex = 1;
+  }
+
+  if (index < 1) {
+    slideIndex = slideshow.length;
+  }
+
   for (i = 0; i < slideshow.length; i++) {
      slideshow[i].style.display = "none";
   }
+
   slideshow[slideIndex-1].style.display = "block";
 }
